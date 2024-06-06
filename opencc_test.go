484f933c88bb12235e549451064bff63a1ea63f0
wangbiao <biao.wang@longbridge.sg>
2024-06-06 17:05:54 +0800

    Fix 免佣

--- opencc_test.go
+++ opencc_test.go
@@ -73,7 +73,7 @@ func TestSelfSpecialHotfix(t *testing.T) {
 	cases := map[string]string{
 		"来自于汇丰，以及汇丰银行，汇入的款项": "來自於滙豐，以及滙豐銀行，匯入的款項",
 		"汇业银行集团": "滙業銀行集團",
-		"佣金":     "佣金",
+		"<p>长桥证券终生免佣，爱你们呦❤️</p>": "<p>长桥证券终生免佣，爱你们呦❤️</p>",
 	}
 
 	assertCases(t, cc, cases)
